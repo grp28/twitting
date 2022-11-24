5666ebb3f8effc7e7f6a8f1e67ed1fb55a48e191
face30 <eloraonah@yahoomail.com>
2022-11-24 23:23:55 +0000

    monitor.sh modified1

--- monitor.sh
+++ monitor.sh
@@ -7,3 +7,7 @@ nagios
 another monitoring tool is PRTG
 it's also very good
 and i higly recommend it for our engineers
+what are your experiences on any of the above tools?
+please share with the group,we need to compare and contrast
+ant is good too
+
